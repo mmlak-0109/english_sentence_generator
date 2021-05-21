fc6cf15b0abb8b19d2014de50adb8b97d1a0dd8b
Matthew Miller <mmlak_0109@yahoo.com>
2021-05-21 23:44:53 +0900

    feat: Add button and text generated on press

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { words, create_simple_sentence } from './utils/_DATA'
 
 export default function App() {
+  const [randomSentence, setRandomSentence] = useState('')
+
+  const onPress = () => (
+    setRandomSentence(create_simple_sentence(words))
+  )
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{randomSentence}</Text>
+      <TouchableOpacity
+        onPress={onPress}>
+          <Text>Generate Random Sentence</Text>
+        </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
